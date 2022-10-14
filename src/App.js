95cd1c8150f74dd50f9d3ba79babf49e9d60850e
nathanjpak <nathan.j.pak@gmail.com>
2022-10-14 16:27:26 -0700

    Move MathJaxContext to index

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import { MathJaxContext } from "better-react-mathjax";
+// import { MathJaxContext } from "better-react-mathjax";
 import GeneratorForm from './components/GeneratorForm';
 
-const config = {
-  loader: { load: ["[tex]/html"] },
-  tex: {
-    packages: { "[+]": ["html"] },
-    inlineMath: [
-      ["$", "$"],
-      ["\\(", "\\)"]
-    ],
-    displayMath: [
-      ["$$", "$$"],
-      ["\\[", "\\]"]
-    ]
-  }
-};
+// const config = {
+//   loader: { load: ["[tex]/html"] },
+//   tex: {
+//     packages: { "[+]": ["html"] },
+//     inlineMath: [
+//       ["$", "$"],
+//       ["\\(", "\\)"]
+//     ],
+//     displayMath: [
+//       ["$$", "$$"],
+//       ["\\[", "\\]"]
+//     ]
+//   }
+// };
 
 const App = () => {
   return (
     <div className="container">
-      <MathJaxContext version={3} config={config} onError={(error) => console.log(error)}>
-        <GeneratorForm />
-      </MathJaxContext>
+      <GeneratorForm />
     </div>    
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { MathJaxContext } from "better-react-mathjax";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const config = {
+  loader: { load: ["[tex]/html"] },
+  tex: {
+    packages: { "[+]": ["html"] },
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"]
+    ],
+    displayMath: [
+      ["$$", "$$"],
+      ["\\[", "\\]"]
+    ]
+  }
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <MathJaxContext version={3} config={config} onError={(error) => console.log(error)}>
+      <App />
+    </MathJaxContext>
   </React.StrictMode>
 );
