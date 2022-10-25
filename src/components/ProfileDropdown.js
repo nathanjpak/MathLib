4be61375370a8d165d66c7a289d0a9a48bcaafa7
nathanjpak <nathan.j.pak@gmail.com>
2022-10-25 11:35:40 -0700

    Add problem sets to dropdown

--- src/components/ProfileDropdown.js
+++ src/components/ProfileDropdown.js
@@ -14,6 +14,8 @@ const ProfileDropdown = () => {
   const { currentUser, setCurrentUser } = useContext(currentUserContext);
   const [ showMenu, setShowMenu ] = useState(false);
   const toggleShowMenu = () => setShowMenu(!showMenu);
+  const [ showSets, setShowSets ] = useState(false);
+  const toggleShowSets = () => setShowSets(!showSets);
 
   const handleLogoutClick = () => {
     logout()
@@ -23,19 +25,31 @@ const ProfileDropdown = () => {
       })
   }
 
+  const handleSetClick = (id) => {
+    navigate(`/s/${id}`);
+  };
+
   const displayName = currentUser.firstName || currentUser.username || currentUser.email;
 
   return (
     <>
-      <button className="login-button" style={{display: "flex", alignItems: "center"}} onClick={toggleShowMenu}>
+      <button className="login-button" style={{display: "flex", alignItems: "center"}} onClick={() => {
+        toggleShowMenu();
+        setShowSets(false);
+      }}>
         <span style={{}}>{displayName}</span>
         {!showMenu && <DownArrow style={{width: "28px", height: "28px", fill: "white"}}/>}
         {showMenu && <UpArrow style={{width: "28px", height: "28px", fill: "white"}}/>}
       </button>
       {showMenu && (<div className="dropdown-menu">
-        <button className="dropdown-menu-button">Your Problem Sets</button>
+        <button className="dropdown-menu-button" onClick={toggleShowSets}>Your Problem Sets</button>
         <button className="dropdown-menu-button">Account Settings</button>
         <button onClick={handleLogoutClick} className="logout-button">Logout</button>
+      </div>)}
+      {showSets && (<div className="set-dropdown-menu">
+        {currentUser.problemSets.map(set => (
+          <button key={set._id} onClick={ () => handleSetClick(set._id) } className="dropdown-menu-button">{set.name}</button>
+        ))}
       </div>)}
     </>
   )
