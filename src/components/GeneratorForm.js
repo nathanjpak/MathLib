b91d287eb1ac1a9676ec70c1b173aa922501158a
nathanjpak <nathan.j.pak@gmail.com>
2022-10-31 14:06:59 -0700

    Update Generator UI part 1

--- src/components/GeneratorForm.js
+++ src/components/GeneratorForm.js
@@ -10,7 +10,7 @@ import GeneratedProblemHeader from "./GeneratedProblemsHeader";
 import ProblemSetModal from "./modals/ProblemSetModal";
 
 const GeneratorForm = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, setValue, getValues } = useForm();
 
   const [problems, setProblems] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -28,22 +28,63 @@ const GeneratorForm = () => {
     }
   };
 
+  // let problemsCount = getValues("problemsCount");
+  const incrementCount = () => {
+    let problemsCount = getValues("problemsCount") || 20;
+    setValue("problemsCount", problemsCount+1);
+  }
+  const decrementCount = () => {
+    let problemsCount = getValues("problemsCount") || 20;
+    setValue("problemsCount", problemsCount-1);
+  }
+
   return (
     <>
     <form className="main-form" 
       onSubmit={handleSubmit(onSubmit)}>
 
-      <div className="form-div-50">
-        <label htmlFor="topic">Topic</label>
-        <select className="input-100" id="topic" {...register("topic")}>
-          <option value="arithmetic">Arithmetic</option>
-          <option value="linearEquations">Linear Equations</option>
-        </select>
+      <div className="input generator-field" style={{width: "50%", alignSelf: "center"}}>
+        Welcome
+      </div>
+
+      <div className="flex-row">
+        <div className="form-div-50">
+          <label htmlFor="topic">Topic</label>
+          <select className="input generator-field" id="topic" {...register("topic")}>
+            <option value="arithmetic">Arithmetic</option>
+            <option value="linearEquations">Linear Equations</option>
+          </select>
+
+          <label htmlFor="problemsCount">Number of Problems</label>
+          <div style={{display: "flex", gap: "2%"}}>
+            <input 
+              className="input generator-field" 
+              {...register("problemsCount", { min: 1, max: 100 })} 
+              type="number" 
+              placeholder="Default: 20"
+              style={{width: "40%"}}
+            />
+            <div className="number-buttons">
+              <button className="button-secondary" style={{flexGrow: 1}} type="button" onClick={incrementCount}>+</button>
+              <button className="button-secondary" style={{flexGrow: 1}} type="button" onClick={decrementCount}>-</button>
+            </div>
+          </div>
+          
+        </div>
+
+        <div className="form-div-50">
+          <label>Filters</label>
+          <div className="input generator-field">
+            Coming Soon <br />
+            {""} <br />
+            {""} <br />
+            {""} <br />
+          </div>
+        </div>
       </div>
       
-      <label htmlFor="problemsCount">Number of Problems</label>
-      <input className="input-100" {...register("problemsCount", { min: 1, max: 100 })} type="number" placeholder="Default: 20" />
-      <button className="generate-button" type="submit">Generate</button>
+      
+      <button className="generator-button" type="submit">Generate</button>
     </form>
     <div className="generated-problems">
       {problems.length>0 && <GeneratedProblemHeader handleSaveClick={toggleIsModalOpen} />}
